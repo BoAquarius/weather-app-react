8d9a658432afae25943b3b7b3ad17941ecc2fce6
Bo Aquarius <aquarius.bozena@gmail.com>
2022-03-17 12:29:31 +0100

    Added GithHub link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ export default function App() {
         <h1>Bozena's React weather app!</h1>
         <Weather />
       </header>
+      <footer>
+        This app has been designed and coded by{" "}
+        <a href="/" target="_blank">
+          Bo Aquarius
+        </a>{" "}
+        and it's{" "}
+        <a
+          href="https://github.com/BoAquarius/weather-app-react"
+          target="_blank"
+        >
+          open-soured on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
