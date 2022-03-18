d4e2fc3b8c65ce5e5c8cc624e3166cb374ff15a7
Bo Aquarius <aquarius.bozena@gmail.com>
2022-03-18 15:41:22 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bozena's React weather app!</h1>
-        <Weather />
-      </header>
-      <footer>
-        This app has been designed and coded by Bo Aquarius and it's{" "}
-        <a
-          href="https://github.com/BoAquarius/weather-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-soured on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Bozena's React weather app!</h1>
+          <Weather />
+        </header>
+        <footer>
+          This app has been designed and coded by Bo Aquarius and it's{" "}
+          <a
+            href="https://github.com/BoAquarius/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-soured on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
