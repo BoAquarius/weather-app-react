2aab53292d0ed16fb1db5c4dd63f3f27128ca274
Bo Aquarius <aquarius.bozena@gmail.com>
2022-05-31 10:28:37 +0200

    Linked the weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       date: "Friday 16:00",
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
     });
